d212d3cd8b8c59f32ae7196e2429d8ba6daa5bdb
Vikram Grover <vik.grover71@gmail.com>
2020-12-28 17:05:58 -0500

    Add final touches

--- flappy_bird.py
+++ flappy_bird.py
@@ -28,18 +28,23 @@ BIRD_IMGS = [IMG_DICT['fb1'], IMG_DICT['fb2'], IMG_DICT['fb3']]
 GROUND_IMG = IMG_DICT['ground']
 PIP_IMG = IMG_DICT['pipe']
 SCENE_IMG = IMG_DICT['scene']
+GROUND_PIPE_VELOCITY = 2
+GROUND_HEIGHT = 50
 WINDOW_WIDTH = SCENE_IMG.get_width()
 WINDOW_HEIGHT = SCENE_IMG.get_height()
-FRAMERATE = 60
+FRAMERATE = 120
+pygame.font.init()
+SCORE_FONT = pygame.font.SysFont("arial", 50)
 
 
 class FlappyBird:
     IMGS = BIRD_IMGS
     MAX_ROTATION = 25
     ROTATION_VEL = 20
-    ANIMATION_TIME = 5
-    JUMP_VEL = -9.8
-    MAX_VEL = 9.8
+    ANIMATION_TIME = 2
+    JUMP_VEL = -2.7
+    FALLING_VEL = 0.04
+    MAX_VEL = 0.6
 
     def __init__(self, x, y):
         self.x = x
@@ -52,7 +57,7 @@ class FlappyBird:
         self.img_index = 0
         self.flapping_dir = 1
         self.curr_img = self.IMGS[0]
-        print(self.IMGS)
+        self.alive = True
 
     def jump(self):
         """
@@ -68,9 +73,9 @@ class FlappyBird:
         """
         self.jump_tick += 0.1
 
-        displacement = self.curr_vel * self.jump_tick
-        self.curr_vel += (0.5 * self.jump_tick)
+        self.curr_vel += (self.FALLING_VEL * self.jump_tick)
         self.curr_vel = min(self.curr_vel, self.MAX_VEL)
+        displacement = self.curr_vel * self.jump_tick
 
         self.y += displacement
 
@@ -85,14 +90,15 @@ class FlappyBird:
         """
         Draws the bird onto the window, handles rotation and flapping of wings
         """
-        self.img_tick += 1
+        if self.alive:
+            self.img_tick += 1
 
         if self.curr_rotation <= -80:
             # the bird is falling and the bird should not be flapping
             self.img_index = 1
             self.curr_img = self.IMGS[1]
             self.img_tick = 0
-        elif (self.img_tick % self.ANIMATION_TIME) == 0:
+        elif (self.img_tick % self.ANIMATION_TIME) == 0 and self.alive:
             # need to switch image
             self.img_tick = 0
             self.img_index += self.flapping_dir
@@ -110,13 +116,93 @@ class FlappyBird:
     def image_mask(self):
         return pygame.mask.from_surface(self.curr_img)
 
+    def die(self):
+        self.alive = False
+        self.curr_vel = 0
+        self.curr_img = self.IMGS[1]
 
-def draw_game(bird, window):
+
+class PipePair:
+    PIPE_GAP = 160
+    VELOCITY = GROUND_PIPE_VELOCITY
+    TOP_PIPE_IMG = pygame.transform.flip(PIP_IMG, False, True)
+    BOTTOM_PIPE_IMG = PIP_IMG
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.top = 0
+        self.bottom = 0
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 350)
+        self.top = self.height - self.TOP_PIPE_IMG.get_height()
+        self.bottom = self.height + self.PIPE_GAP
+
+    def move(self):
+        self.x -= self.VELOCITY
+
+    def draw(self, window):
+        window.blit(self.TOP_PIPE_IMG, (self.x, self.top))
+        window.blit(self.BOTTOM_PIPE_IMG, (self.x, self.bottom))
+
+    def collision(self, bird):
+        bird_img_mask = bird.image_mask()
+        top_pipe_mask = pygame.mask.from_surface(self.TOP_PIPE_IMG)
+        bottom_pipe_mask = pygame.mask.from_surface(self.BOTTOM_PIPE_IMG)
+
+        bird_top_pipe_offset = (self.x - bird.x, self.top - round(bird.y))
+        bird_bottom_pipe_offset = (
+            self.x - bird.x, self.bottom - round(bird.y))
+
+        top_overlap = bird_img_mask.overlap(
+            top_pipe_mask, bird_top_pipe_offset)
+        bottom_overlap = bird_img_mask.overlap(
+            bottom_pipe_mask, bird_bottom_pipe_offset)
+
+        return (top_overlap or bottom_overlap)
+
+
+class Ground:
+    VELOCITY = GROUND_PIPE_VELOCITY
+    WIDTH = GROUND_IMG.get_width()
+    IMG = GROUND_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VELOCITY
+        self.x2 -= self.VELOCITY
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, window):
+        window.blit(self.IMG, (self.x1, self.y))
+        window.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_game(bird, ground, pipe_pairs, window, total_score):
     """
     Draws the whole screen
     """
     window.blit(SCENE_IMG, (0, 0))
+    for pipe_pair in pipe_pairs:
+        pipe_pair.draw(window)
+
+    score = SCORE_FONT.render(str(total_score), 1, (255, 255, 255))
+    window.blit(score, (WINDOW_WIDTH//2 -
+                        score.get_width()//2, 10))
     bird.draw(window)
+    ground.draw(window)
     pygame.display.update()
 
 
@@ -128,24 +214,69 @@ def main():
     flappy_bird = FlappyBird(
         WINDOW_WIDTH//2 - BIRD_IMGS[0].get_width()//2, WINDOW_HEIGHT//2 - BIRD_IMGS[0].get_height()//2)
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    ground = Ground(WINDOW_HEIGHT - GROUND_HEIGHT)
     ticker = pygame.time.Clock()
+    total_score = 0
+    pipe_pairs = [PipePair(WINDOW_WIDTH + 100)]
+    game_over = False
 
-    running = True
     # main game loop
-    while running:
+    while True:
         ticker.tick(FRAMERATE)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
+                quit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE and flappy_bird.alive:
                     flappy_bird.jump()
+                elif event.key == pygame.K_r:
+                    flappy_bird = FlappyBird(
+                        WINDOW_WIDTH//2 - BIRD_IMGS[0].get_width()//2, WINDOW_HEIGHT//2 - BIRD_IMGS[0].get_height()//2)
+                    window = pygame.display.set_mode(
+                        (WINDOW_WIDTH, WINDOW_HEIGHT))
+                    ground = Ground(WINDOW_HEIGHT - GROUND_HEIGHT)
+                    ticker = pygame.time.Clock()
+                    total_score = 0
+                    pipe_pairs = [PipePair(WINDOW_WIDTH + 100)]
+                    game_over = False
+                    continue
 
-        flappy_bird.move()
-        draw_game(flappy_bird, window)
+        if not game_over:
+            if flappy_bird.y < WINDOW_HEIGHT - GROUND_HEIGHT - BIRD_IMGS[0].get_height():
+                flappy_bird.move()
+            elif flappy_bird.alive:
+                flappy_bird.die()
+            else:
+                game_over = True
 
-    pygame.quit()
-    quit()
+            new_pipe = False
+            i = 0
+            while i < len(pipe_pairs) and flappy_bird.alive:
+                pipe_pair = pipe_pairs[i]
+
+                if pipe_pair.collision(flappy_bird):
+                    flappy_bird.die()
+
+                if pipe_pair.x + pipe_pair.TOP_PIPE_IMG.get_width() < 0:
+                    pipe_pairs.pop(i)
+                    continue
+
+                if not pipe_pair.passed and pipe_pair.x < flappy_bird.x:
+                    pipe_pair.passed = True
+                    new_pipe = True
+
+                pipe_pair.move()
+                i += 1
+
+            if new_pipe:
+                total_score += 1
+                pipe_pairs.append(PipePair(WINDOW_WIDTH + 100))
+
+            if flappy_bird.alive:
+                ground.move()
+
+        draw_game(flappy_bird, ground, pipe_pairs, window, total_score)
 
 
 # calling main function
